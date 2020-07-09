9585b514a3f3d1c5c08be7065a533a7161fb31fb
Michelle Chen <mich.chen.94@gmail.com>
2020-07-08 21:51:21 -0700

    created new function day_summary that takes the number day, and .txt file. Cleaned up code. All works

--- produce_summary.py
+++ produce_summary.py
@@ -1,3 +1,22 @@
+"""
+    print("Day 1") # prints "day 1" as a title
+    the_file = open("um-deliveries-20140519.txt") # opens text file and stores into the_file as each line 
+    for line in the_file: # for each line in the_file
+        line = line.rstrip() # strip the white space from the end of each line
+        words = line.split('|') # words is a list of strings of sections that are split by '|'
+
+        melon = words[0] # the first string section of the list word is stored in melon
+        count = words[0] # the first string section of the list word is stored in count
+        amount = words[0] # the first string section of the list word is stored in amount
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount)) 
+    the_file.close() # close opened file
+    # since melon, count, and amount are all the same index element of words, therefore we are only printing that one item repeatedly in the print statement.
+
+    """
+
+
 def lines_into_list(line):
     """ takes file with lines and strips, and splits to return a list of item words """
 
@@ -15,37 +34,14 @@ def print_info(item_list):
     print("Delivered {} {}s for total of ${}".format(melon, count, amount))
 
 
-
-def day_1():
-    print("Day 1") # prints "day 1" as a title
-    the_file = open("um-deliveries-20140519.txt") # opens text file and stores into the_file as each line 
-    for line in the_file: # for each line in the_file
-        line = line.rstrip() # strip the white space from the end of each line
-        words = line.split('|') # words is a list of strings of sections that are split by '|'
-
-        melon = words[0] # the first string section of the list word is stored in melon
-        count = words[0] # the first string section of the list word is stored in count
-        amount = words[0] # the first string section of the list word is stored in amount
-
-        print("Delivered {} {}s for total of ${}".format(
-            count, melon, amount)) 
-    the_file.close() # close opened file
-    # since melon, count, and amount are all the same index element of words, therefore we are only printing that one item repeatedly in the print statement.
-
-def day_2():
-    print("Day 2")
-    the_file = open("um-deliveries-20140520.txt")
+def day_summary(day, report):
+    print(f'Day {day}')
+    the_file = open(report)
     for line in the_file:
         words_list = lines_into_list(line)
         print_info(words_list)
     the_file.close()
 
-def day_3():
-    print("Day 3")
-    the_file = open("um-deliveries-20140521.txt")
-    for line in the_file:
-        words_list = lines_into_list(line)
-        print_info(words_list)
-    the_file.close()
-
-day_2()
+day_summary(1, "um-deliveries-20140519.txt")
+day_summary(2, "um-deliveries-20140520.txt")
+day_summary(3, "um-deliveries-20140521.txt")
